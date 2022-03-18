03ca55a31308ec7fbe036271715421acaa5ab5ff
Stephen Pak <stephenjpak@gmail.com>
2022-03-18 16:01:51 -0700

    3-18-22-401

--- assets/js/game.js
+++ assets/js/game.js
@@ -195,9 +195,23 @@ var shop = function () {
 
 /* GAME INFORMATION / VARIABLES */
 
+// function to set name
+var getPlayerName = function () {
+  var name = '';
+
+  // ***************************************
+  // ADD LOOP BELOW WITH PROMPT AND CONDITION
+  // ***************************************
+  while (name === '' || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
